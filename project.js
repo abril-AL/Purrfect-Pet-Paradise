4d4e5fba10fc4ae39b0ae7178278bd557f76034f
abril <abril124@g.ucla.edu>
2024-05-21 18:04:29 -0700

    skeleton code

---
+++ project.js
@@ -0,0 +1,46 @@
+import { defs, tiny } from './examples/common.js';
+
+const {
+    Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene,
+} = tiny;
+
+export class Assignment3 extends Scene {
+    constructor() {
+        // constructor(): Scenes begin by populating initial values like the Shapes and Materials they'll need.
+        super();
+
+        // At the beginning of our program, load one of each of these shape definitions onto the GPU.
+        this.shapes = {
+            //add shapes here
+        };
+
+        // *** Materials
+        this.materials = {
+            //add materials
+        }
+
+        //this.initial_camera_location = Mat4.look_at(vec3(0, 10, 20), vec3(0, 0, 0), vec3(0, 1, 0));
+        this.initial_camera_location = Mat4.look_at(vec3(0, 10, 20), vec3(0, 0, 0), vec3(0, 1, 0));
+    }
+
+    make_control_panel() {
+        // add buttons here
+    }
+
+    display(context, program_state) {
+        // display():  Called once per frame of animation.
+        // Setup -- This part sets up the scene's overall camera matrix, projection matrix, and lights:
+        if (!context.scratchpad.controls) {
+            this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
+            // Define the global camera and projection matrices, which are stored in program_state.
+            program_state.set_camera(this.initial_camera_location);
+        }
+
+        program_state.projection_transform = Mat4.perspective(Math.PI / 4, context.width / context.height, .1, 1000);
+        const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
+        let model_transform = Mat4.identity();
+
+        // add scene here
+
+    }
+}
