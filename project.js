84141348fc8d2ef9baa6c02d74684cfe1e689385
George Zevada <gjzevada@gmail.com>
2024-06-05 15:57:00 -0700

    Cat chases the ball whichever way it goes

--- project.js
+++ project.js
@@ -556,6 +556,8 @@ class Base_Scene extends Scene {
 
         Object.assign(this, {time_accumulator: 0, time_scale: 1, t: 0, dt: 1 / 20, bodies: [], steps_taken: 0});
         this.direction = 0;
+        this.xdir = 0;
+        this.zdir = 0;
         const bump = new defs.Fake_Bump_Map(1);
         const textured = new defs.Textured_Phong(1);
         this.materials = {
@@ -762,6 +764,8 @@ export class Project extends Base_Scene {
         this.new_line();
         this.key_triggered_button("Play with ball", ["x"], () => {
             this.direction = Math.floor(Math.random() * 2);
+            this.xdir = Math.random();
+            this.zdir = Math.random();
             this.ball = !this.ball;
             this.brush_out = false;
             this.cat_feed = false;
@@ -805,16 +809,16 @@ export class Project extends Base_Scene {
         // update_state():  Override the base time-stepping code to say what this particular
         // scene should do to its bodies every frame -- including applying forces.
         // Generate additional moving bodies if there ever aren't enough:
-        while (this.bodies.length < 1) {
+        while (this.bodies.length < 2) {
             if (this.direction == 0) {
                 this.bodies.push(new Ball(this.shapes.sphere, this.random_color(), vec3(1, 1, 1))
                     .emplace(Mat4.translation(-2.5, 15, 3),
-                        vec3(Math.random(), -1, Math.random()).times(3), 0));
+                        vec3(this.xdir, -1, this.zdir).times(3), 0));
             }
             else{
                 this.bodies.push(new Ball(this.shapes.sphere, this.random_color(), vec3(1, 1, 1))
                     .emplace(Mat4.translation(-2.5, 15, 3),
-                        vec3(-Math.random(), -1, Math.random()).times(3), 0));
+                        vec3(-this.xdir, -1, this.zdir).times(3), 0));
             }
         }
 
@@ -830,11 +834,12 @@ export class Project extends Base_Scene {
         if (this.bodies[0].linear_velocity[1] < 0.01 && this.bodies[0].center[1] < -5.95){
             this.bodies[0].linear_velocity[1] = 0;
         }
+
     }
 
     display(context, program_state) {
         super.display(context, program_state);
-        const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
+        const t = program_state.animation_time / 1000, cdt = program_state.animation_delta_time / 100;
         let model_transform = Mat4.identity();
 
         let cat_mat;
@@ -917,17 +922,30 @@ export class Project extends Base_Scene {
             }else if (this.ball){
 
                 this.happy_i -= 0.1;
+                let rot = Math.atan(Math.sqrt(Math.pow(this.bodies[0].center[0],2))/Math.sqrt(Math.pow(this.bodies[0].center[2],2)));
 
-                model_transform = Mat4.identity().times(Mat4.scale(1 / 2, 1 / 2, 1 / 2)).times(Mat4.translation(0, -7, -2));
-                this.shapes.cat.draw_stand(context, program_state, model_transform,
-                    cat_mat, cat_mat, cat_mat,
-                    cat_mat, cat_mat, cat_mat);//fix paramaters later
-                //eyes
-                this.shapes.cube.draw(context, program_state, model_transform.times(Mat4.scale(2.2, 2, 0.1))
-                    .times(Mat4.translation(-2.3, 1, 45)), eye_texture);
-                //nose
-                this.shapes.cube.draw(context, program_state, model_transform.times(Mat4.scale(1 / 3, 1 / 3, 1 / (20 / 3)))
-                    .times(Mat4.translation(-15.5, 4.4, 37)).times(Mat4.scale(1.2, 1.2, 1.2)), this.materials.nose_tile);
+                if (this.direction == 0) {
+                    model_transform = Mat4.identity().times(Mat4.rotation(rot, 0, 1, 0))
+                    model_transform = model_transform.times(Mat4.scale(1 / 2, 1 / 2, 1 / 2)).times(Mat4.translation(0, -7, -2+t*4));
+                    this.shapes.cat.draw_stand(context, program_state, model_transform,
+                        cat_mat, cat_mat, cat_mat,
+                        cat_mat, cat_mat, cat_mat);
+                    this.shapes.cube.draw(context, program_state, model_transform.times(Mat4.scale(2.2, 2, 0.1))
+                        .times(Mat4.translation(-2.3, 1, 45)), eye_texture);
+                    this.shapes.cube.draw(context, program_state, model_transform.times(Mat4.scale(1 / 3, 1 / 3, 1 / (20 / 3)))
+                        .times(Mat4.translation(-15.5, 4.4, 37)).times(Mat4.scale(1.2, 1.2, 1.2)), this.materials.nose_tile);
+                }
+                else{
+                    model_transform = Mat4.identity().times(Mat4.rotation(-rot, 0, 1, 0))
+                    model_transform = model_transform.times(Mat4.scale(1 / 2, 1 / 2, 1 / 2)).times(Mat4.translation(0, -7, -2+t*4));
+                    this.shapes.cat.draw_stand(context, program_state, model_transform,
+                        cat_mat, cat_mat, cat_mat,
+                        cat_mat, cat_mat, cat_mat);
+                    this.shapes.cube.draw(context, program_state, model_transform.times(Mat4.scale(2.2, 2, 0.1))
+                        .times(Mat4.translation(-2.3, 1, 45)), eye_texture);
+                    this.shapes.cube.draw(context, program_state, model_transform.times(Mat4.scale(1 / 3, 1 / 3, 1 / (20 / 3)))
+                        .times(Mat4.translation(-15.5, 4.4, 37)).times(Mat4.scale(1.2, 1.2, 1.2)), this.materials.nose_tile);
+                }
 
             }
             else {
@@ -947,6 +965,7 @@ export class Project extends Base_Scene {
 
             if(this.ball == false){
                 this.bodies = []
+                program_state.animation_time = 0
             }
 
             this.happiness_level += 0.1;
